b62e089743a7eaa93ab74e1765f20d1edd5ce116
notyourbear <pracenberg@gmail.com>
2018-03-22 21:12:15 -0700

    fns for title generator added

---
+++ dev/titleGenerator/fns.js
@@ -0,0 +1,102 @@
+import seedrandom from 'seedrandom';
+
+const sample = ({ array, seed }) => {
+  const rng = seed ? seedrandom(seed) : seedrandom();
+  const index = Math.floor(rng() * array.length - 1);
+  return array[index];
+}
+
+const MarkovNode = ({entry, exit, value}) => {
+  const state = {
+    entry: false,
+    exit: false,
+    value: null
+  };
+
+  return Object.assign(state, { entry, exit, value});
+};
+
+const arrayMaker = ({sentence, amount, type='word'}) => {
+  if (type === 'word') return sentence.split(' ');
+}
+
+const addToState = ({amount, array, state = {}}) => {
+  array.forEach((item, i) => {
+    let nexti = i + 1;
+    let previ = i - 1;
+
+    const nodeState = {
+      entry: false,
+      exit: false,
+      value: item
+    };
+
+    if (previ < 0) nodeState.entry = true;
+    if (nexti >= array.length) nodeState.exit = true;
+
+    if (state[item] === undefined) state[item] = [];
+    state[item].push(MarkovNode(nodeState));
+  });
+
+  return state;
+}
+
+const getItem = state => sample(sample(state));
+const switcher = direction => (direction === 'prev' ? 'next' : 'prev');
+
+const createChain = ({state, amount, type}) => {
+  let haveStart = false;
+  let haveEnd = false;
+  let direction = 'prev';
+  let i = 0;
+
+  let item = getItem(state);
+  while (item.next === true && item.prev === true) {
+    item = getItem(state);
+  }
+
+  let sentence = '';
+
+  while (i < amount) {
+    direction = switcher(direction);
+    if (haveStart === true && haveEnd === true) break;
+    if (haveStart && direction === 'prev') continue;
+    if (haveEnd && direction === 'next') continue;
+
+    item = getItem(state)
+    sentence = direction === 'prev' ? `${item.value} ${sentence}` : `${sentence} ${item.value}`;
+
+    haveStart = item.entry === true;
+    haveEnd = item.exit === true;
+
+    i++;
+  }
+
+  if (haveStart === false) {
+    let item = getItem(state);
+    while(item.entry !== true) {
+      item = getItem(state);
+    }
+
+    sentence = `${item.prev} ${sentence}`;
+  }
+
+  if (haveEnd === false) {
+    let item = getItem(state);
+    while(item.exit !== true) {
+      item = getItem(state);
+    }
+    sentence = `${sentence} ${item.next}`;
+  }
+
+  return sentence;
+};
+
+export default {
+  sample,
+  MarkovNode,
+  arrayMaker,
+  createChain,
+  getItem,
+  switcher
+}
