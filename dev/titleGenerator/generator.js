3d7db40bf98bd33537d0cbfb189b89eaa4a6c839
notyourbear <pracenberg@gmail.com>
2018-03-24 21:15:55 -0700

    title generator works!

---
+++ dev/titleGenerator/generator.js
@@ -0,0 +1,28 @@
+import fns from './fns.js';
+import titles from './titles.js';
+
+const populateState = ({array, state}) => {
+  array.forEach(input => {
+    let a = fns.arrayMaker({input});
+    fns.addToState({array: a, state});
+  });
+};
+
+const create = ({state}) => (options = { amount: 2 }) => {
+  let { amount, seed } = options;
+  return fns.createChain({state, amount, seed});
+}
+
+
+const Markov = () => {
+  const state = {
+    entry: [],
+    exit: [],
+    node: {}
+  };
+
+  populateState({array:titles, state});
+  return  { create: create({state}) };
+};
+
+export default Markov;
---
+++ dev/titleGenerator/generator.test.js
@@ -0,0 +1,42 @@
+import Markov from './generator.js';
+
+describe('Markov', () => {
+  test('create works', () => {
+    const gen = Markov();
+    const val = gen.create();
+    expect(typeof val === 'string').toBe(true);
+  });
+
+  test('returns ten unique items', () => {
+    const gen = Markov();
+    let unique = true;
+    let prev;
+    for(let i = 0; i < 10; i++) {
+      const val = gen.create();
+      if (i !== 0) {
+        unique = prev !== val;
+      }
+      if (unique === false) break;
+      prev = val;
+      console.log(val)
+    }
+
+    expect(unique).toBe(true);
+  });
+
+  test('returns the same item ten times', () => {
+    const gen = Markov();
+    let same = true;
+    let prev;
+    for(let i = 0; i < 10; i++) {
+      const val = gen.create({seed: '3'});
+      if (i !== 0) {
+        same = prev === val;
+      }
+      if (same === false) break;
+      prev = val;
+    }
+
+    expect(same).toBe(true);
+  });
+});
