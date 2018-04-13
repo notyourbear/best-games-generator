e952084f13dd763223df213b6f363b54a2032a9a
notyourbear <pracenberg@gmail.com>
2018-04-13 12:06:24 -0700

    systems generator works

--- dev/systemsGenerator/generator.js
+++ dev/systemsGenerator/generator.js
@@ -1,27 +1,42 @@
-import systems from './systems.js';
-import fns from '../titleGenerator/fns.js';
+import systemSchema from './systems.js';
+import fns from './fns.js';
 
 const generator = (obj = {}) => {
-  let {amount, seed} = obj;
-  const consoleArray = Object.values(systems);
-  const generation = fns.sample({array: consoleArray, seed}).slice();
+  let {amount, seed, title} = obj;
 
-  if (amount === undefined) {
-    const amountArray = [];
-    for (let i = 0; i < generation.length; i++) {
-      amountArray.push(i + 1);
+  const consoleTypes = ['portable', 'console', 'mobile', 'pc'];
+  const consoleType = fns.sample({array: consoleTypes, seed});
+
+  const platformOptions = fns.sample({array: systemSchema[consoleType], seed});
+  const {systems, releaseDates} = platformOptions;
+
+  const result = {
+    consoleType,
+    releaseDate: fns.between({array: releaseDates, seed})
+  }
+
+  if (title) {
+    let singlePlatform = fns.platformCentric({title});
+    if (singlePlatform !== false) {
+      result.systems = singlePlatform;
+      return result;
     }
-
-    amount = fns.sample({array: amountArray, seed});
   }
 
-  const result = [];
+  // --------platform gen assuming its title agnostic--------
+  if (amount === undefined) {
+    const amountArray = [1, systems.length];
+    amount = fns.between({array: amountArray, seed});
+  }
+
+  const plats = [];
   for (let i = 0; i < amount; i++) {
-    let console = fns.sample({array: generation, seed});
-    let consoleIndex = generation.indexOf(console);
-    result.push(generation.splice(consoleIndex, 1).pop());
+    let console = fns.sample({array: systems, seed});
+    let consoleIndex = systems.indexOf(console);
+    plats.push(systems.splice(consoleIndex, 1).pop());
   }
 
+  result.systems = plats;
   return result;
 };
 
--- dev/systemsGenerator/generator.test.js
+++ dev/systemsGenerator/generator.test.js
@@ -1,21 +1,27 @@
 import generator from './generator.js';
 
 describe('Title Generator', () => {
-  test('outputs an array of consoles', () => {
-    const arr = generator();
-    expect(Array.isArray(arr)).toBe(true);
+  test('outputs a stack of console info', () => {
+    const { consoleType, releaseDate, systems } = generator();
+    expect(Array.isArray(systems)).toBe(true);
+    expect(['portable', 'console', 'mobile', 'pc'].includes(consoleType)).toBe(true);
+    expect(releaseDate <= 2018).toBe(true);
   });
 
-  test('outputs an array of 1 console', () => {
-    const arr = generator({amount: 1});
-    expect(Array.isArray(arr)).toBe(true);
-    expect(arr.length).toEqual(1);
+  test('outputs an array of 1 console for systems', () => {
+    const { consoleType, releaseDate, systems } = generator({amount: 1});
+    expect(Array.isArray(systems)).toBe(true);
+    expect(systems.length).toEqual(1);
+    expect(['portable', 'console', 'mobile', 'pc'].includes(consoleType)).toBe(true);
+    expect(releaseDate <= 2018).toBe(true);
   });
 
-  test('outputs the same array of 2 console', () => {
-    const arr = generator({amount: 2, seed: '234'});
-    expect(Array.isArray(arr)).toBe(true);
-    expect(arr.length).toEqual(2);
-    expect(arr).toEqual(expect.arrayContaining([ 'Genesis', 'Neo Geo' ]))
+  test('outputs a specific input of 2 consoles', () => {
+    const { consoleType, releaseDate, systems } = generator({amount: 2, seed: '231'});
+    expect(Array.isArray(systems)).toBe(true);
+    expect(systems.length).toEqual(2);
+    expect(systems).toEqual(expect.arrayContaining([ 'Saturn', 'Jaguar' ]))
+    expect(consoleType).toBe('console');
+    expect(releaseDate).toBe(1995);
   });
 });
