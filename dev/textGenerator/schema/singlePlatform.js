2b2e9fc8861e9b890c8d7fd4f32fdabbb4b566cd
notyourbear <pracenberg@gmail.com>
2018-04-19 22:22:46 -0700

    spelling eror

--- dev/textGenerator/schema/singlePlatform.js
+++ dev/textGenerator/schema/singlePlatform.js
@@ -1,7 +1,7 @@
 export default {
   "singlePlatform-0": "::game.S.title:: pushed the ::game.S.platform:: to its limits.",
   "singlePlatform-1": "::game.S.title:: was a standout of weird ::game.S.platform:: releases.",
-  "singlePlatform-2": "::game.S.title:: was in someways the last hurrah on the ::game.S.platform::",
+  "singlePlatform-2": "::game.S.title:: was in someways the last hurrah on the ::game.S.platform::.",
   "singlePlatform-3": "::game.S.title:: was a must-own for ::game.S.genre::-lovers on the ::game.S.platform::.",
   "singlePlatform-4": "::game.S.title|possessive:: tight gameplay made it a standout of the then-aging ::game.S.platform::.",
   "singlePlatform-5": "When released in ::game.S.releaseDate::, ::game.S.title:: set a new world record as the fastest-selling ::game.S.platform:: game, resonating quickly with players.",
