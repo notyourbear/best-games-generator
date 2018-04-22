4f565eba2a5afcddea2457ba33cc9d13ba6fe158
notyourbear <pracenberg@gmail.com>
2018-04-21 21:03:41 -0700

    about!

---
+++ README.md
@@ -0,0 +1,5 @@
+[500 best games of all time](https://www.polygon.com/features/2017/11/27/16158276/polygon-500-best-games-of-all-time-500-401).
+
+To pay homage, I thought it would be a fun project to create a generative version. The titles of the video games are generated via a simple Markov Chain, which takes in a large list of actual video game titles and then spits out a new combination thereof. The descriptions are actually mangled together from Polygon's feature, using simplified versions of its descriptions as the building blocks of the text.
+
+The generator uses the title of the site as the seed for randomization; this makes a list easy to share, should you find a made up list that you think is worth sharing. As example, in a universe where the website Polygonal exists, it's 11th best game of all time will always be a game called _Offworld Deadly Company_, a game I would most definitively play.
--- dev/templatingFns/homepage/template.js
+++ dev/templatingFns/homepage/template.js
@@ -4,7 +4,16 @@ const homePageTemplate = (data = {}) => {
 
   document.title = `The ${number} best games of all time - ${title}`;
 
-  return ` <div class='form-container'>
+  return `
+    <div class='offscreen'>
+      <h3>What Even Is This?</h3>
+      <p>To celebrate their fifth birthday, the website Polygon decided to rank the 500 best games of all time.</p>
+      <p>To pay homage, I thought it would be a fun project to create a generative version. </p>
+      <p>The generator uses the title of the site as the seed for randomization; this makes a list easy to share, should you find a made up list that you think is worth sharing.</p>
+      <p>As example, in a universe where the website Polygonal exists, it's 11th best game of all time will always be a game called <span class="emphasis">Offworld Deadly Company</span>, a game I would most definitively play.</p>
+    </div>
+    <div class='form-container'>
+      <div class='about'>What even is this?</div>
       <div class="background-logo"></div>
       <form class='form create-best-games'>
         <input id='title-input' type='text' name='title' value='${title}' autofocus>
@@ -16,7 +25,7 @@ const homePageTemplate = (data = {}) => {
         <span> After weeks of voting and arguments, we’re ready to present our choices </span>
         <input id='submit-input' type='submit' name='submit' value='View Now'>
      </form>
-     </div>`;
+    </div>`;
 }
 
 
