f801fdb6143aa03e7cd209ca6136fea42b3d771c
Patrick Racenberg <pracenberg@gmail.com>
2021-01-01 17:29:54 -0800

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 module.exports = (env, args) => ({
-  entry: "./dev",
-  mode: "none",
-  devtool: "none",
+  entry: './dev',
+  mode: 'none',
+  devtool: 'none',
   output: {
-    library: "bundle",
-    libraryTarget: "umd",
-    filename: "bundle.js",
-    globalObject: "this"
-  }
+    library: 'bundle',
+    libraryTarget: 'umd',
+    filename: 'docs/bundle.js',
+    globalObject: 'this',
+  },
 });
